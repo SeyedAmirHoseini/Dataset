db2a1ba8b0a34522ce75b639ae9e9c893307c752
SeyedAmirHoseini <seyedamirhosseeini@gmail.com>
2025-04-06 13:49:24 +0330

    First Commit

---
+++ master/download_dataset.py
@@ -0,0 +1,23 @@
+from .utils import *
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+custom_dir, force_update = "C:\\Users\\MesterComputer\\Desktop\\dataset\\Venv\\datasets", False
+
+
+@dataset_decorator(custom_dir)
+def dataset_downloader(force_update: bool, version_path: str) -> str:
+
+    dataset_name = "suvroo/ai-for-elderly-care-and-support"
+
+    json_creator()
+    
+    api = KaggleApi()
+    api.authenticate() 
+    if force_update:
+        print(f"📥 Dar hal download dataset {version_path}")
+        api.dataset_download_files(dataset_name, path=version_path, unzip=True)
+
+        print("✅ Dataset ba movafaghiyat nasb shod!")
+
+    return version_path
